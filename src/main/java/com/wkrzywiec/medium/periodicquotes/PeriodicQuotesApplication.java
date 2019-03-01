11e13ec9975688153d43614df4cf15bc814c860a
Wojciech Krzywiec <wojciechkrzywiec@gmail.com>
2019-03-01 05:29:27 +0100

    Initial commit

---
+++ src/main/java/com/wkrzywiec/medium/periodicquotes/PeriodicQuotesApplication.java
@@ -0,0 +1,14 @@
+package com.wkrzywiec.medium.periodicquotes;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeriodicQuotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeriodicQuotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wkrzywiec/medium/periodicquotes/PeriodicQuotesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wkrzywiec.medium.periodicquotes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PeriodicQuotesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
