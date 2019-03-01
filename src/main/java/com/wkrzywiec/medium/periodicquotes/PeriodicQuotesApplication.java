ab8278fffa5530c36d04c329d224d11466d2d6b5
Wojciech Krzywiec <wojciechkrzywiec@gmail.com>
2019-03-01 06:12:05 +0100

    Add RxJava dependencies

--- src/main/java/com/wkrzywiec/medium/periodicquotes/PeriodicQuotesApplication.java
+++ src/main/java/com/wkrzywiec/medium/periodicquotes/PeriodicQuotesApplication.java
@@ -1,10 +1,12 @@
 package com.wkrzywiec.medium.periodicquotes;
 
+import io.reactivex.Flowable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class PeriodicQuotesApplication implements CommandLineRunner {
@@ -20,5 +22,7 @@ public class PeriodicQuotesApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 
 		LOG.info("Test");
+
+		Flowable.just("Hello world").subscribe(System.out::println);
 	}
 }
