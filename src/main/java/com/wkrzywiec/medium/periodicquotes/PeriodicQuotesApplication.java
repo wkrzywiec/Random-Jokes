cd71f52975bf1b20f3c14f8a4e3d565862dbc603
Wojciech Krzywiec <wojciechkrzywiec@gmail.com>
2019-03-01 05:34:47 +0100

    console application

--- src/main/java/com/wkrzywiec/medium/periodicquotes/PeriodicQuotesApplication.java
+++ src/main/java/com/wkrzywiec/medium/periodicquotes/PeriodicQuotesApplication.java
@@ -1,14 +1,24 @@
 package com.wkrzywiec.medium.periodicquotes;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PeriodicQuotesApplication {
+public class PeriodicQuotesApplication implements CommandLineRunner {
+
+	private static Logger LOG = LoggerFactory
+			.getLogger(PeriodicQuotesApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(PeriodicQuotesApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		LOG.info("Test");
+	}
 }
