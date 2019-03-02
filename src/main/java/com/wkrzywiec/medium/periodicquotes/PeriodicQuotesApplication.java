fd3f60c4ceeb2ff2281cb8061a4bec02ae258c10
Wojciech Krzywiec <wojciechkrzywiec@gmail.com>
2019-03-02 18:33:01 +0100

    Add simple observable that emits an event each 2 second

--- src/main/java/com/wkrzywiec/medium/periodicquotes/PeriodicQuotesApplication.java
+++ src/main/java/com/wkrzywiec/medium/periodicquotes/PeriodicQuotesApplication.java
@@ -1,11 +1,15 @@
 package com.wkrzywiec.medium.periodicquotes;
 
 import io.reactivex.Flowable;
+import io.reactivex.Observable;
+import io.reactivex.schedulers.Schedulers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.concurrent.TimeUnit;
 
 
 @SpringBootApplication
@@ -24,5 +28,11 @@ public class PeriodicQuotesApplication implements CommandLineRunner {
 		LOG.info("Test");
 
 		Flowable.just("Hello world").subscribe(System.out::println);
+
+		Observable.interval(2, TimeUnit.SECONDS, Schedulers.io())
+				.observeOn(Schedulers.newThread())
+				.subscribe(s -> LOG.info("Task"));
+
+		Thread.sleep(10000);
 	}
 }
